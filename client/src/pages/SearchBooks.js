6730e993aa7544d661c2a06da08995a31b0cc97e
deluus polycarp <pdeluus@gmail.com>
2022-04-22 09:49:38 -0700

    deploying

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,10 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutation';
+import { GET_ME } from '../utils/queries';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -14,6 +18,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,48 +1,44 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
-type Book {
-    _id: ID!
-    bookId: String
-    authors: [String]
-    # authors: String
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-type User {
-    _id: ID!
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
-input savedBook {
-    description: String
-    title: String
-    bookId: String
-    image: String
-    link: String
-    authors: [String]
-}
-type Query {
-    me: User  
-  }
-type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: savedBook!): User
-    removeBook(bookId: ID!): User
-}
-type Auth {
-    token: ID!
-    user: User
-  }
+const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    input BookInput {
+        bookId: ID
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        deleteBook(bookId: ID!): User
+    }
 `;
 
-
-
-// export the typeDefs
-module.exports = typeDefs;
+module.exports = typeDefs; 

07d55f510ab97cfed5b0f07eb0494860af1e225d
deluus polycarp <pdeluus@gmail.com>
2022-04-22 09:58:06 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-machine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,

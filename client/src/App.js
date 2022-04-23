61be5245bf539688a1d9cfbd73b17a7711b95416
deluus polycarp <pdeluus@gmail.com>
2022-04-23 14:13:31 -0700

    troubleshoot sign up/in form save books

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from "@apollo/client/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
+const httpLink = createHttpLink({
+  uri: '/graphql'
 });
+
+const authLink = setContext((_, {headers}) => {
+
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ``,
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
@@ -18,7 +35,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<SearchBooks/>} />
-          <Route exact path='/saved' component={SavedBooks} />
+          <Route exact path='/saved' element={<SavedBooks/>} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Routes>
       </>
